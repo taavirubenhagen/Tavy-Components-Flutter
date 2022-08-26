8958a4db9d765d915537e74122226e5b921751e7
Taavi Rübenhagen <112179792+taavirubenhagen@users.noreply.github.com>
2022-08-26 18:08:39 +0200

    No version specified

--- deploy.ps1
+++ deploy.ps1
@@ -1,24 +1,9 @@
 <# This script commits changes to GitHub for iOS deployment #>
 
 
-param ([String]$os = "ios", [String]$m = "No version specified", [String]$pname = "project")
+param ([String]$os = "ios", [String]$m = "No version specified")
 
 
 
 
-if ($os -eq "ios" -or $os -eq "both") {
-  git add .
-  git commit . -m $m
-  git push -u origin main
-}
-
-if ($os -eq "android" -or $os -eq "both") {
-  flutter build appbundle
-}
-
-
-try {
-  C:\\Users\User\SCRIPTS\Credit-Tavy "Deployed $pname for Flutter."
-} catch {
-  Write-Output Deployed.
-}
+C:\\Users\User\SCRIPTS\Deploy-Flutter -os $os -m $m -pname "Tavy Components"
