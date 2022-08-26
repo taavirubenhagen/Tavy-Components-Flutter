b59d8d3f8c4a89c799128de16cd3cd765a36695e
Taavi Rübenhagen <112179792+taavirubenhagen@users.noreply.github.com>
2022-08-26 16:55:04 +0200

    2.0.0

---
+++ deploy.ps1
@@ -0,0 +1,23 @@
+<# This script commits changes to GitHub for iOS deployment #>
+
+
+param ([String]$OperatingSystem = "ios")
+
+
+
+
+if ($OperatingSystem -eq "ios" -or $OperatingSystem -eq "both") {
+  git commit deploy.ps1 pubspec.yaml ./lib ./test -m "2.0.0"
+  git push -u origin master
+}
+
+if ($OperatingSystem -eq "android" -or $OperatingSystem -eq "both") {
+  flutter build appbundle
+}
+
+
+try {
+  C:\\Users\User\SCRIPTS\Credit-Tavy "Deployed Tavy Components for Flutter."
+} catch {
+  Write-Output Deployed.
+}
