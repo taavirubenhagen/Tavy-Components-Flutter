c97e32ad6ab40c4afa3fd614563db793ee86f2ba
Taavi Rübenhagen <112179792+taavirubenhagen@users.noreply.github.com>
2023-09-23 12:30:56 +0200

    Untitled

--- deploy.ps1
+++
@@ -1,9 +0,0 @@
-<# This script commits changes to GitHub for iOS deployment #>
-
-
-param ([String]$os = "ios", [String]$m = "No version specified")
-
-
-
-
-C:\\Users\User\SCRIPTS\Deploy-Flutter -os $os -m $m -pname "Tavy Components"
